499e30fdbbf4b54334f76e1c529bbe2535448918
Edwin <edwin.paul.73@gmail.com>
2022-09-30 17:17:24 -0500

    add libraries and generate environment configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  await app.listen(app.get(ConfigService).get('port'));
 }
+
 bootstrap();
