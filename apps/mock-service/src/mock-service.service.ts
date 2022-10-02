88444afb6306fe2d5bf3966e80b2b12d9ba33575
Edwin <edwin.paul.73@gmail.com>
2022-10-02 13:22:23 -0500

    add mock service project

--- apps/mock-service/src/mock-service.service.ts
+++ apps/mock-service/src/mock-service.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { RepositoryResponseDto } from './dto/repository-response.dto';
 
+const TEST_DATA = [
+  { id: 1, state: 604 },
+  { id: 2, state: 605 },
+  { id: 3, state: 606 },
+];
+
 @Injectable()
 export class MockServiceService {
   getRepositoryStatus(): RepositoryResponseDto {
-    return new RepositoryResponseDto([
-      { id: 1, state: 604 },
-      { id: 2, state: 605 },
-      { id: 3, state: 606 },
-    ]);
+    return new RepositoryResponseDto(TEST_DATA);
   }
 }
